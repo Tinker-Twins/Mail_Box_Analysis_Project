78e8cf37f99598a5ae62e3c8cda38d4e15d777fc
Samak Twins <samaktwins@gmail.com>
2019-01-15 21:18:17 +0530

    Add files via upload

---
+++ Email_Extractor.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+conn = sqlite3.connect('Email_DB.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+
+cur.execute('''
+CREATE TABLE Counts (email TEXT, count INTEGER)''')
+
+fname = input('Enter file name: ')
+if (len(fname) < 1): fname = 'Mail_Box.txt'
+fh = open(fname)
+print('Writing to Database...')
+for line in fh:
+    if not line.startswith('From: '): continue
+    pieces = line.split()
+    email = pieces[1]
+    cur.execute('SELECT count FROM Counts WHERE email = ? ', (email,))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('''INSERT INTO Counts (email, count)
+                VALUES (?, 1)''', (email,))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE email = ?',
+                    (email,))
+    conn.commit()
+
+sqlstr = 'SELECT email, count FROM Counts ORDER BY count DESC LIMIT 10'
+
+for row in cur.execute(sqlstr):
+    print(str(row[0]), row[1])
+print('Done!')
+cur.close()
